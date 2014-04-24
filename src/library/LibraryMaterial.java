0cf12e857e92706420ceb2c08e88659976eca944
Tommy <Tommy@10.113.2.130>
2014-04-24 10:12:27 -0500

    test

--- src/library/LibraryMaterial.java
+++ src/library/LibraryMaterial.java
@@ -8,8 +8,67 @@ package library;
 
 /**
  *
- * @author Tommy
+ * @author Tommy Yang
  */
 public class LibraryMaterial {
+    
+    private String bookAuthor;
+    private String bookTitle;
+    private double bookPrice;
+    private int publicationYear;
+    
+    /**
+     * Default Constructor
+     */
+    public LibraryMaterial() {
+    }
+    
+    /**
+     * 
+     * @param author The author of the library item
+     * @param title The title of the library item
+     * @param price The price of the library item
+     * @param year The year of publication of the library item
+     */
+    public LibraryMaterial(String author, String title, double price, int year) {
+        author = bookAuthor;
+        title = bookTitle;
+        price = bookPrice;
+        year = publicationYear;
+    }
+    
+    /**
+     * 
+     * @return Author of the library item
+     */
+    public String getAuthor() {
+        return bookAuthor;
+    }
+    
+    /**
+     * 
+     * @return Title of library item
+     */
+    public String getTitle() {
+        return bookTitle;
+    }
+    
+    /**
+     * 
+     * @return Price of library item
+     */
+    public double getPrice() {
+        return bookPrice;
+    }
+    
+    /**
+     * 
+     * @return Publication Year of library item
+     */
+    public int getYear() {
+        return publicationYear;
+    }
+    
+    
     
 }
