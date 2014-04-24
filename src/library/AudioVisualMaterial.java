1d30f652210210b14646cc4c02e961948b584bf0
kouxiong88 <kxng88@gmail.com>
2014-04-23 22:06:45 -0500

    Added constructors and methods along with javadoc documentation.

--- src/library/AudioVisualMaterial.java
+++ src/library/AudioVisualMaterial.java
@@ -6,10 +6,102 @@
 
 package library;
 
+import java.applet.AudioClip;
+import java.awt.Image;
+
 /**
+ * Audio and visual material Class
  *
  * @author Kou Xiong
  */
-public class AudioVisualMaterial {
-    //test tl
+public class AudioVisualMaterial
+{
+    
+    protected String avType;
+    protected Image avCover;
+    protected AudioClip avSound;
+    
+    /**
+     * Default audio and visual material constructor.
+     * 
+     */
+    public AudioVisualMaterial ()
+    {
+        
+    }
+    
+    /**
+     * Audio and visual material constructor.
+     * @param _avType AudioVisual type
+     * @param _avCover AudioVisual cover
+     * @param _avSound AudioVisual sound clip
+     */
+    public AudioVisualMaterial (String _avType, Image _avCover, AudioClip _avSound)
+    {
+        avType = _avType;
+        avCover = _avCover;
+        avSound = _avSound;
+    }
+    
+    /**
+     * Gets the type of audio and visual material.
+     * @return a string specifying the type of material
+     */
+    public String getType ()
+    {
+        return avType;
+    }
+    
+    /**
+     * Sets the type of audio and visual material.
+     * @param _type the type of material
+     */
+    public void setType (String _type)
+    {
+        avType = _type;
+    }
+    
+    /**
+     * Displays the audio and visual material's cover. 
+     * @return an image of the audio and visual material
+     */
+    public Image displayCover ()
+    {
+        return avCover;
+    }
+    
+    /**
+     * Sets the cover of the audio and visual material.
+     * @param _cover the cover image
+     */
+    public void setCover (Image _cover)
+    {
+        avCover = _cover;
+    }
+    
+    /**
+     * Plays the audio and visual material sound clip.
+     */
+    public void playSound ()
+    {
+        avSound.play();
+    }
+    
+    /**
+     * Sets the sound of the audio and visual material.
+     * @param _sound the sound clip
+     */
+    public void setSound (AudioClip _sound)
+    {
+        avSound = _sound;
+    }
+    
+    /**
+     * Displays the audio and visual material info.
+     */
+    public void displayInfo ()
+    {
+        
+    }
+    
 }
