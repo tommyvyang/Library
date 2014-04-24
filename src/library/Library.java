3c936787a7919d6f07cedcd520e3fc504d02ca78
Tommy <Tommy@10.113.2.130>
2014-04-24 12:52:57 -0500

    test

--- src/library/Library.java
+++ src/library/Library.java
@@ -17,6 +17,8 @@ public class Library {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Librarian gui = new Librarian();
+        gui.setVisible(true);
     }
     
 }
