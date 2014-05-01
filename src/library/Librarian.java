3cfbdafc459d70e16762f134589b9ba9b7a50f76
Tommy <Tommy@WhiteBanana>
2014-04-30 22:04:31 -0500

    Merge origin/master
    Conflicts:
    	src/library/Librarian.java

--- src/library/Librarian.java
+++ src/library/Librarian.java
@@ -80,6 +80,7 @@ public class Librarian extends javax.swing.JFrame {
         imgPanel = new javax.swing.JPanel();
         imgLabel = new javax.swing.JLabel();
         exitButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -137,11 +138,14 @@ public class Librarian extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("AR BERKLEY", 1, 36)); // NOI18N
+        jLabel1.setText("The Coolest Library in Town");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(39, 39, 39)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
@@ -157,13 +161,19 @@ public class Librarian extends javax.swing.JFrame {
                 .addGap(77, 77, 77))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(exitButton)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(exitButton)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(134, 134, 134))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(53, 53, 53)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(imgPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
@@ -191,6 +201,9 @@ public class Librarian extends javax.swing.JFrame {
         
         
         if(bookRadio.isSelected() == true) {
+            jukebox.stopSound();
+            jane.stopSound();
+            light.stopSound();
             switch(itemList.getSelectedIndex()){
                 case 0:
                     infoArea.setText("");
@@ -233,6 +246,9 @@ public class Librarian extends javax.swing.JFrame {
         }
         
         if(dvdRadio.isSelected() == true) {
+            jukebox.stopSound();
+            jane.stopSound();
+            light.stopSound();
             switch(itemList.getSelectedIndex()) {
                 case 0: 
                     infoArea.setText(""); 
@@ -431,6 +447,7 @@ public class Librarian extends javax.swing.JFrame {
     private javax.swing.JPanel imgPanel;
     private javax.swing.JTextArea infoArea;
     private javax.swing.JComboBox itemList;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
